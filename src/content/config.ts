5899e5d33c570b3ed2a0e75e3cddf87234de6642
Phillip Gourley <shootex@gmail.com>
2025-02-28 00:19:30 +0000

    Adding blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,19 @@ const docs = defineCollection({
   }),
 });
 
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    author: z.string().optional(),
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
 export const collections = {
   docs,
+  blog,
 };
