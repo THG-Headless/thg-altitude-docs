c6a2f385089053d021fb3b7b5234d263aec6e27f
Phillip Gourley <shootex@gmail.com>
2025-03-04 12:04:28 +0000

    Stylistic changes

--- src/components/Glow.tsx
+++ src/components/Glow.tsx
@@ -35,14 +35,14 @@ const Glow = React.forwardRef<
   >
     <div
       className={cn(
-        "absolute left-1/2 h-[512px] w-[60%] -translate-x-1/2 scale-[2.5] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_hsla(var(--brand-foreground)/.5)_10%,_hsla(var(--brand-foreground)/0)_60%)] sm:h-[512px]",
+        "absolute left-1/2 h-[512px] w-[40%] -translate-x-1/2 scale-[2.5] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_hsla(var(--brand-foreground)/.5)_10%,_hsla(var(--brand-foreground)/0)_60%)] sm:h-[512px]",
         variant === "center" && "-translate-y-1/2",
         variant === "bottom" && "bottom-0",
       )}
     />
     <div
       className={cn(
-        "absolute left-1/2 h-[256px] w-[40%] -translate-x-1/2 scale-[2] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_hsla(var(--brand)/.3)_10%,_hsla(var(--brand-foreground)/0)_60%)] sm:h-[256px]",
+        "absolute left-1/2 h-[256px] w-[100%] -translate-x-1/2 scale-[2] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_hsla(var(--brand)/.3)_10%,_hsla(var(--brand-foreground)/0)_60%)] sm:h-[256px]",
         variant === "center" && "-translate-y-1/2",
         variant === "bottom" && "bottom-0"
       )}
