5c21f4fe16be4192e880b8bdfea8eee30d096105
Phillip Gourley <shootex@gmail.com>
2025-03-07 17:36:54 +0000

    fixing static on blog

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ import typography from '@tailwindcss/typography';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: {
-    files: ['./src/**/*!(*.stories|*.spec|*.test).{js,ts,tsx,astro,md,mdx,html}'],
+    files: ['./src/**/*!.{js,ts,tsx,astro,md,mdx,html}'],
   },
   darkMode: 'class', // Allows toggling to 'light' mode manually
   theme: {
